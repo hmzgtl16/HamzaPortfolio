b8312204c383c8303fc50ac2f61005d8051a6ebb
GATTAL Hamza <hmzgtl16@gmail.com>
2026-01-02 20:43:19 +0100

    Add GSAP library and implement DotGrid component with interactive canvas effects

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
 import './App.css'
+import DotGrid from "./components/backgrounds/dot-grid/DotGrid.tsx";
 
 function App() {
 
-  return (
-      <h1 className="text-3xl font-bold underline">
-          Hello HAMZA!
-      </h1>
-  )
+    return (
+
+    <div className="bg-zinc-950 w-full h-screen relativee">
+        <DotGrid
+            dotSize={3.5}
+            gap={25}
+            baseColor="#ffffff"
+            activeColor="#1a0863"
+            proximity={150}
+            shockRadius={250}
+            shockStrength={15}
+            resistance={2000}
+            returnDuration={2}
+        />
+    </div>
+    )
 }
 
 export default App
