3e2299bb88e15b4c8ec3470aaaeedb6c1fefbee0
GATTAL Hamza <hmzgtl16@gmail.com>
2026-01-06 23:58:59 +0100

    Optimize build configuration: add manual chunk splitting for better code splitting and performance

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,28 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-      react(),
-      tailwindcss(),
-  ],
+    plugins: [
+        react(),
+        tailwindcss(),
+    ],
     resolve: {
         alias: {
             "@": path.resolve(__dirname, "./src"),
         },
     },
+    build: {
+        rolldownOptions: {
+            output: {
+                manualChunks: (id) => {
+                    if (id.includes('node_modules')) {
+                        return id.toString().split('node_modules/')[1].split('/')[0].toString();
+                    }
+                }
+            }
+        }
+    }
 })
