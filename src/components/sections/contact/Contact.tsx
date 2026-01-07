562d6081fef731b890556dba448969665638c741
GATTAL Hamza <hmzgtl16@gmail.com>
2026-01-07 14:12:29 +0100

    Remove BlurText and FadeContent components: replace with semantic HTML elements for simplified structure and improved maintainability.

--- src/components/sections/contact/Contact.tsx
+++ src/components/sections/contact/Contact.tsx
@@ -2,8 +2,6 @@ import {Element} from 'react-scroll';
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faGithub, faLinkedin, faTelegram, faWhatsapp} from "@fortawesome/free-brands-svg-icons";
 import {faEnvelope} from "@fortawesome/free-solid-svg-icons";
-import BlurText from "@/components/texts/blur-text/BlurText.tsx";
-import FadeContent from "@/components/animations/fade-content/FadeContent.tsx";
 
 function Contact() {
 
@@ -45,38 +43,28 @@ function Contact() {
             <section className="flex flex-col align-middle justify-center h-full">
                 <div className="mx-auto max-w-2xl px-4 py-16 space-y-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
                     <div className="mx-auto max-w-3xl lg:text-center">
-                        <BlurText
-                            text='Get in touch'
-                            delay={100}
-                            className="text-4xl font-serif font-bold tracking-tight text-pretty sm:text-5xl capitalize"
-                        />
-                        <BlurText
-                            text='Don’t hesitate to get in touch if you have any questions or would like to connect.'
-                            delay={100}
-                            className="mt-6 text-2xl font-sans font-medium text-foreground/60 tracking-wide"
-                        />
+                        <h2 className="text-4xl font-serif font-bold tracking-tight text-pretty sm:text-5xl capitalize">
+                            Get in touch
+                        </h2>
+                        <p className="mt-6 text-2xl font-sans font-medium text-foreground/60 tracking-wide">
+                            Don’t hesitate to get in touch if you have any questions or would like to connect
+                        </p>
                     </div>
                     <div className="mx-auto max-w-3xl lg:text-center">
-                        <FadeContent
-                            delay={1.8}
-                            duration={1.75}
-                            threshold={0.25}
-                        >
-                            <div className="flex flex-wrap justify-center gap-x-6 gap-y-4">
-                                {socialLinks.map((link) => (
-                                    <a
-                                        key={link.id}
-                                        href={link.url}
-                                        className="w-18 h-18 p-4 inline-flex items-center justify-center gap-x-4 border border-neutral-800 bg-neutral-900 rounded-full hover:bg-neutral-800 hover:scale-105 transition-all duration-300"
-                                        aria-label={link.label}
-                                        target="_blank"
-                                        rel="noopener noreferrer"
-                                    >
-                                        <FontAwesomeIcon icon={link.icon} size="2xl"/>
-                                    </a>
-                                ))}
-                            </div>
-                        </FadeContent>
+                        <div className="flex flex-wrap justify-center gap-x-6 gap-y-4">
+                            {socialLinks.map((link) => (
+                                <a
+                                    key={link.id}
+                                    href={link.url}
+                                    className="w-18 h-18 p-4 inline-flex items-center justify-center gap-x-4 border border-neutral-800 bg-neutral-900 rounded-full hover:bg-neutral-800 hover:scale-105 transition-all duration-300"
+                                    aria-label={link.label}
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                >
+                                    <FontAwesomeIcon icon={link.icon} size="2xl"/>
+                                </a>
+                            ))}
+                        </div>
                     </div>
                 </div>
             </section>
--- src/components/sections/services/Services.tsx
+++ src/components/sections/services/Services.tsx
@@ -1,8 +1,6 @@
 import {Element} from 'react-scroll';
 import ServiceCard from "@/components/cards/service-card/ServiceCard.tsx";
 import {faCode, faMobileScreen, faServer} from "@fortawesome/free-solid-svg-icons";
-import BlurText from "@/components/texts/blur-text/BlurText.tsx";
-import FadeContent from "@/components/animations/fade-content/FadeContent.tsx";
 
 const services = [
     {
@@ -30,28 +28,20 @@ function Services() {
         <Element name="services" className="w-full">
             <section className="flex flex-col align-middle justify-center h-full">
                 <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
-                    <BlurText
-                        text='What I do?'
-                        delay={100}
-                        className="text-4xl font-serif font-bold text-center tracking-tight text-pretty sm:text-5xl capitalize"
-                    />
-                    <FadeContent
-                        delay={1.0}
-                        duration={1.7}
-                        threshold={0.25}
-                    >
-                        <div
-                            className="mt-8 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:gap-x-8">
-                            {services.map((service) => (
-                                <ServiceCard
-                                    key={service.id}
-                                    content={service}
-                                    className="group"
-                                >
-                                </ServiceCard>
-                            ))}
-                        </div>
-                    </FadeContent>
+                    <h2 className="text-4xl font-serif font-bold text-center tracking-tight text-pretty sm:text-5xl capitalize">
+                        What I do?
+                    </h2>
+                    <div
+                        className="mt-8 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:gap-x-8">
+                        {services.map((service) => (
+                            <ServiceCard
+                                key={service.id}
+                                content={service}
+                                className="group"
+                            >
+                            </ServiceCard>
+                        ))}
+                    </div>
                 </div>
             </section>
         </Element>
